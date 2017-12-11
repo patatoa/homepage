917742340e944354242754fe1a6a4ddf35b3fb52
Patrick <patatoa@gmail.com>
2017-12-11 12:36:57 -0600

    moved static files to public

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var port = process.env.port || 5000;
 var express = require("express");
 
 var app = express();
-app.use(express.static(__dirname ));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/home/", function(request, response){
-    response.sendFile(__dirname + "/home.html");
+    response.sendFile(__dirname + "/public/home.html");
 });
 
-app.get("/", function(request, response){
-    response.send("test");
-});
+// app.get("/", function(request, response){
+//     response.send("test");
+// });
 
 app.listen(port, function(){
     console.log(__dirname);
