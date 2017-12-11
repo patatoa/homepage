75c2e0003845cbb0a6cb2aede6ab731d22393965
Patrick <patatoa@gmail.com>
2017-12-11 14:04:17 -0600

    Refactored route paths in server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var port = process.env.PORT || 5000;
-var path = require("path")
+var path = require("path");
 var express = require("express");
 
 var app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/home/", function(request, response){
-    response.sendFile(__dirname + "/public/home.html");
+    response.sendFile(path.join(__dirname, "public", "home.html"));
 });
 app.get("/work/", function(request, response){
-    response.sendFile(__dirname + "/public/work.html");
+    response.sendFile(path.join(__dirname, "public", "work.html"));
 });
 
 app.get("/", function(request, response){
