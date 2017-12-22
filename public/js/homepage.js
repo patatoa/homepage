0c197a71f505eb7fd7cffbfc8dea7f604014d1e2
Patrick <patatoa@gmail.com>
2017-12-22 11:26:15 -0600

    Added hotkey hints
    Added the key name to the link class. This allows me to show the real
    key label lext to the list items as a hint. In case I forget.
    
    Added small styling to be used with this hint.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,46 +3,47 @@ var home = ["Home", "First things first, Amarilliss", "Good morning, Batman", "I
 var title = home[Math.floor(Math.random() * home.length)];
 
 class link{
-    constructor(id, url, hotkey){
+    constructor(id, url, hotkey, key){
         this.id = id;
         this.url = url;
         this.hotkey = hotkey;
+        this.key = key;
     }
 }
 
 var linkList = [
     // hotkey t
-    new link("todoist", "https://en.todoist.com/", 84),
+    new link("todoist", "https://en.todoist.com/", 84, "t"),
     // hotkey i
-    new link("inbox",  "https://inbox.google.com", 73),
+    new link("inbox",  "https://inbox.google.com", 73, "i"),
     // hotkey r
-    new link("reddit",  "https://www.reddit.com", 82),
+    new link("reddit",  "https://www.reddit.com", 82, "r"),
     // hotkey b
-    new link("r-nba", "https://www.reddit.com/r/nba", 66),
+    new link("r-nba", "https://www.reddit.com/r/nba", 66, "b"),
     // hotkey l
-    new link("trello", "https://trello.com/", 76),
+    new link("trello", "https://trello.com/", 76, "l"),
     // hotkey y
-    new link("youtube", "https://www.youtube.com", 89),
+    new link("youtube", "https://www.youtube.com", 89, "y"),
     // hotkey f
-    new link("facebook", "https://www.youtube.com", 70),
+    new link("facebook", "https://www.youtube.com", 70, "f"),
     // hotkey m
-    new link("music", "https://music.google.com", 77),
+    new link("music", "https://music.google.com", 77, "m"),
     // hotkey o
-    new link("onenote", "https://onedrive.live.com/edit.aspx?resid=81E5ACFF337707F3!130&cid=81e5acff337707f3&app=OneNote", 79),
+    new link("onenote", "https://onedrive.live.com/edit.aspx?resid=81E5ACFF337707F3!130&cid=81e5acff337707f3&app=OneNote", 79, "o"),
     // hotkey k
-    new link("keep", "https://keep.google.com", 75),
+    new link("keep", "https://keep.google.com", 75, "k"),
     // hotkey 4
-    new link("4chan-b", "https://boards.4chan.org/b/", 52),
+    new link("4chan-b", "https://boards.4chan.org/b/", 52, "4"),
     // hotkey u
-    new link("4chan-mu", "https://boards.4chan.org/mu/", 85),
+    new link("4chan-mu", "https://boards.4chan.org/mu/", 85, "u"),
     // hotkey g
-    new link("4chan-g", "https://boards.4chan.org/g/", 71),
+    new link("4chan-g", "https://boards.4chan.org/g/", 71, "g"),
     // hotkey a
-    new link("amazon", "http://smile.Amazon.com", 65),
+    new link("amazon", "http://smile.Amazon.com", 65, "a"),
     // hotkey v
-    new link("amazonVideo", "http://www.amazon.com/Prime-Instant-Video/b/ref=sd_allcat_aiv_piv?ie=UTF8&node=2676882011", 86),
+    new link("amazonVideo", "http://www.amazon.com/Prime-Instant-Video/b/ref=sd_allcat_aiv_piv?ie=UTF8&node=2676882011", 86, "v"),
     // hotkey p
-    new link("pitchfork", "http://www.Pitchfork.com", 80),
+    new link("pitchfork", "http://www.Pitchfork.com", 80, "p"),
 ];
 
 $(document).foundation();
@@ -62,7 +63,7 @@ $(document).ready(function () {
 
     for(var i =0; i < linkList.length; i++){
         let link = linkList[i];
-        $("#"+link.id).attr("href", link.url);
+        $("#"+link.id).attr("href", link.url).append("<small>("+link.key+")</small>");
     }
 
     $(document).keyup(function(e){
