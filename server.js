fb1678ab0286a8d462904534a7e5ba8cea53e451
Patrick <patatoa@gmail.com>
2018-06-23 19:02:26 -0500

    Compiling Foundation Sass

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var port = process.env.PORT || 5000;
 var path = require("path");
+var sass = require("node-sass-middleware");
 var express = require("express");
 
 var app = express();
+app.use(
+    sass({
+        src: __dirname +  '/node_modules/foundation/scss',
+        dest: __dirname + '/public/css/',
+        prefix: '/css/',
+        debug: true
+    })
+);
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/content/', express.static(__dirname + '/node_modules/foundation/scss'));
+app.use('/js/foundation/', express.static(__dirname + '/node_modules/foundation/js/foundation'));
+app.use('/js/shared/', express.static(__dirname + '/node_modules/foundation/js/vendor'));
 
 app.get("/home/", function(request, response){
     response.sendFile(path.join(__dirname, "public", "home.html"));
