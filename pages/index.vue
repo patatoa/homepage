13d3d09b0c2da61241cbed8b336e85411275f384
Patrick <patatoa@gmail.com>
2018-07-06 22:18:59 -0500

    Beginning to implement vue template
    Copied (hard-coded) bookmarks to pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,45 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        homepage
-      </h1>
-      <h2 class="subtitle">
-        My sensational Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+    <div class="cell medium-4" data-equalizer-watch>
+        <h2>Not fun</h2>
+        <ul>
+            <li>
+                <img src="/img/inbox.png"/>
+                <a id="inbox">Inbox</a>
+            </li>
+            <li>
+                <img src="/img/drive.ico"/>
+                <a id="drive">Drive</a>
+            </li>
+            <li>
+                <img src="/img/onedrive.png"/>
+                <a id="onedrive">OneDrive</a>
+            </li>
+            <li>
+                <img src="/img/onenote.png"/>
+                <a id="onenote">OneNote</a>
+            </li>
+            <li>
+                <img src="/img/keep.png"/>
+                <a id="keep">Keep</a>
+            </li>
+            <li>
+                <img src="/img/todoist.png"/>
+                <a id="todoist">Todoist</a>
+            </li>
+            <li>
+                <img src="/img/trello.png"/>
+                <a id="trello">Trello</a>
+            </li>
+            <li>
+                <img src="/img/pluralsight.ico"/>
+                <a id="pluralsight">PluralSight</a>
+            </li>
+            <li>
+                <img src="/img/pocket.png"/>
+                <a id="pocket">Pocket</a>
+            </li>
+        </ul>
     </div>
-  </section>
 </template>
 
 <script>
@@ -25,35 +51,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
-</style>
